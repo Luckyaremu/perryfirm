ffcec3eb4ff2888ffb265e266ef29799c08154d9
Luckyaremu <aremu.baba70@gmail.com>
2021-03-19 12:30:10 +0100

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>check it out</h1>
-      </header>
+     <Navbar/>
     </div>
   );
 }
