a2f1e7f7e9efeb1b00d1e6c54e5733ca0fa89e9d
Luckyaremu <aremu.baba70@gmail.com>
2021-03-25 14:16:45 +0100

    contact fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Home from "./pages";
 import contact from "./pages/contact";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <Router className="App">
+      <Navbar />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/contact" component={contact} exact />
       </Switch>
+      <Footer />
     </Router>
   );
 }
