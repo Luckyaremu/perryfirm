de81ed09071217003e72f53447bebfa55a85cba7
Luckyaremu <aremu.baba70@gmail.com>
2021-03-25 11:44:23 +0100

    styling service section

--- src/component/Services/ServicesElements.js
+++ src/component/Services/ServicesElements.js
@@ -43,12 +43,13 @@ padding: 0 50px;
 
 export const ServicesCard = styled.div`
 background: #fff;
+height: 300px;
+width: 300px;
 display: flex;
 flex-direction: column;
 justify-content: flex-start;
 align-items: center;
 border-radius: 10px;
-max-height: 340px;
 padding: 16px;
 box-shodow: 0 1px 3px rgba(0,0,0,0.2);
 transition: all 0.2s ease-in-out;
@@ -79,7 +80,7 @@ margin-bottom: 64px;
 
 export const ServicesH2 = styled.h2`
 font-size: 1rem;
-margin-bottom: 10px;
+margin-bottom: 20px;
 `;
 
 
--- src/component/Services/index.js
+++ src/component/Services/index.js
@@ -4,7 +4,7 @@ import Icon2 from "../../images/security.svg";
 import Icon3 from "../../images/document.svg";
 import Icon4 from "../../images/svg-2.svg";
 import Icon5 from "../../images/supply.svg";
-import Icon6 from "../../images/svg-3.svg";
+import Icon6 from "../../images/approval.svg";
 import { ServicesContainer, ServicesWrapper, ServicesH1, ServicesCard,
      ServicesIcon, ServicesH2, ServicesP} from "./ServicesElements.js";
 
@@ -14,42 +14,41 @@ const Services = () => {
             <ServicesH1> OUR SERVICES </ServicesH1>
             <ServicesWrapper>
                 <ServicesCard>
-                    <ServicesIcon src={Icon1}/>
-                    <ServicesH2>Naval Clearance Of Vessels</ServicesH2>
-                    <ServicesP>We help reduce fees and process your services</ServicesP>
-                </ServicesCard>
-
-                <ServicesCard>
-                    <ServicesIcon src={Icon2}/>
-                    <ServicesH2>Naval Armed Escort</ServicesH2>
-                    <ServicesP>We help reduce fees and process your services</ServicesP>
-                </ServicesCard>
-
-                <ServicesCard>
                     <ServicesIcon src={Icon3}/>
                     <ServicesH2>DPR Imports Permit </ServicesH2>
-                    <ServicesH2>& Nimansa Vessels Registration</ServicesH2>
-                    
+                    <ServicesP>@Trena general services, we can easily get you a DPR import permit to enable your perform your business efficiently within your region of permit</ServicesP>
                 </ServicesCard>
                 
                 <ServicesCard>
                     <ServicesIcon src={Icon4}/>
-                    <ServicesH2>Marine Logistics</ServicesH2>
-                    <ServicesP>We help reduce fees and process your services</ServicesP>
-                </ServicesCard>
+                    <ServicesH2>Marine Logistics & Haulage</ServicesH2>
+                    <ServicesP>@Trena general services, we can clear your vessels within 48 hours with affordable negotiations as agreed with your team</ServicesP>
+               </ServicesCard>
 
                 
                 <ServicesCard>
                     <ServicesIcon src={Icon5}/>
                     <ServicesH2>Bunker Supplies</ServicesH2>
-                    <ServicesP>We help reduce fees and process your services</ServicesP>
+                    <ServicesP>@Trena general services, we can help you do your supplies at every point or place you want it</ServicesP>
                 </ServicesCard>
 
                 
                 <ServicesCard>
                     <ServicesIcon src={Icon6}/>
-                    <ServicesH2>Haulage</ServicesH2>
-                    <ServicesP>We help reduce fees and process your services</ServicesP>
+                    <ServicesH2>Nimasa Vessels Registration</ServicesH2>
+                    <ServicesP>@Trena general services, reach out to us to make your registration easy, efficient and affordable</ServicesP>
+                </ServicesCard>
+
+                <ServicesCard>
+                    <ServicesIcon src={Icon1}/>
+                    <ServicesH2>Naval Clearance Of Vessels</ServicesH2>
+                    <ServicesP>@Trena general services, we can clear your vessels within 48 hours with affordable negotiations as agreed with your team</ServicesP>
+                </ServicesCard>
+
+                <ServicesCard>
+                    <ServicesIcon src={Icon2}/>
+                    <ServicesH2>Naval Armed Escort</ServicesH2>
+                    <ServicesP>@Trena general services, we offer top security to your vessels and making sure all goods and are well protected</ServicesP>
                 </ServicesCard>
 
             </ServicesWrapper>
