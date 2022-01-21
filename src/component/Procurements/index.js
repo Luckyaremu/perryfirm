b0111741d0fdd32ba62966598d61f0afe90d143c
Luckyaremu <aremu.baba70@gmail.com>
2022-01-21 12:39:38 +0100

    updated services

--- src/component/Procurements/index.js
+++ src/component/Procurements/index.js
@@ -7,16 +7,16 @@ const Services = () => {
             <ServicesH1> we also deals on </ServicesH1>
             <ServicesWrapper>
                 <ServicesCard>    
-                    <ServicesP>Supply of safety and protetion equipment</ServicesP>
-                    <ServicesP>Supply of motor pumps and breaks</ServicesP>
-                    <ServicesP>Supply of communications equipments and accessories such as rounters, moderms, hubs, cables etc</ServicesP>
-                    <ServicesP>Transportaion and supply of computers equipments and life chat headset</ServicesP>
+                    <ServicesP>Supply of safety and protetion equipment.</ServicesP>
+                    <ServicesP>Supply of motor pumps and breaks.</ServicesP>
+                    <ServicesP>Supply of communications equipments and accessories such as rounters, moderms, hubs, cables etc.</ServicesP>
+                    <ServicesP>Transportaion and supply of computers equipments and life chat headset.</ServicesP>
                 </ServicesCard>
                 <ServicesCard>    
-                    <ServicesP>Supply of gaskets, joints, packings and tighten ms </ServicesP>
-                    <ServicesP>Supply of ball valves, subsea valves, safety valves, relief valves abd bursting discs </ServicesP>
-                    <ServicesP>Supply and installations of furniture at offshore locations </ServicesP>
-                    <ServicesP>Supply of flanges, elbows, fittings hangers and pipe supports. </ServicesP>
+                    <ServicesP>Supply of gaskets, joints, packings and tighten ms.</ServicesP>
+                    <ServicesP>Supply of ball valves, subsea valves, safety valves, relief valves abd bursting discs.</ServicesP>
+                    <ServicesP>Supply and installations of furniture at offshore locations.</ServicesP>
+                    <ServicesP>Supply of flanges, elbows, fittings hangers and pipe supports.</ServicesP>
                 </ServicesCard>
                 <NavBtn>
                    <NavBtnLink to="/contact">Contact Us Today</NavBtnLink>
